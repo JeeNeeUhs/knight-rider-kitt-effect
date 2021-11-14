792f8f087ad7ea2e454b941fca2eea88032696b5
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2021-11-14 19:37:32 +0300

    Update KnightRiderKittEffect.ino

--- KnightRiderKittEffect.ino
+++ KnightRiderKittEffect.ino
@@ -9,13 +9,13 @@
 #define buzzer 11 // buzzer 
 #define pot A0 // potantiometer
 int fast = 50 ; //flash speed
-int ledler[] = {2,3,4,5,6,7,8,9,10}; //led pins
+int leds[] = {2,3,4,5,6,7,8,9,10}; //led pins
 
 void setup() {
   pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
   for(int i=0; i<9; i++){
-  pinMode(ledler[i], OUTPUT);
+  pinMode(leds[i], OUTPUT);
   }
 }
 
@@ -28,24 +28,24 @@ void loop() {
     fast = analogRead(pot);
     Serial.println(fast/4);
     digitalWrite(buzzer, LOW);
-    digitalWrite(ledler[i], HIGH);
+    digitalWrite(leds[i], HIGH);
     delay(fast/4);
     fast = analogRead(pot);
     Serial.println(fast/4);
     digitalWrite(buzzer, HIGH);
-    digitalWrite(ledler[i], LOW);
+    digitalWrite(leds[i], LOW);
   }
 
   for(int j=8; j>-1; j--){
     fast = analogRead(pot);
     Serial.println(fast/4);
     digitalWrite(buzzer, LOW);
-    digitalWrite(ledler[j], HIGH);
+    digitalWrite(leds[j], HIGH);
     delay(fast/4);
     fast = analogRead(pot);
     Serial.println(fast/4);
     digitalWrite(buzzer, HIGH);
-    digitalWrite(ledler[j], LOW);
+    digitalWrite(leds[j], LOW);
   }
 
 }
